148dc029101925b30eafb0201b052665bfd2c3df
Hassan Shaikh <shaikh.hassan01@outlook.com>
2024-05-24 11:21:59 -0400

    created the connection to mysql db

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require("mysql2");
 const express = require("express");
 
 const PORT = process.env.PORT || 3001;
@@ -6,6 +7,17 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//Connect to Database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "P@ssw0rd",
+    database: "election",
+  },
+  console.log("Connected to the election database.")
+);
 
 /* Test express 
 app.get("/", (req, res) => {
@@ -17,8 +29,8 @@ app.get("/", (req, res) => {
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
-    res.status(404).end();
-})
+  res.status(404).end();
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
