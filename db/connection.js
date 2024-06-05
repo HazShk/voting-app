4bd0fd70f178eb4568d19e94ffd3f0c9ee5ebb5c
Hassan Shaikh <shaikh.hassan01@outlook.com>
2024-06-05 11:32:06 -0400

    completed all API routes

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,12 @@
-const mysql = require("mysql2");
+const mysql = require('mysql2');
 
-// Connect to database
 const db = mysql.createConnection({
-    host: 'localhost',
-    // Your MySQL username,
-    user: 'root',
-    // Your MySQL password 
-    password: 'P@ssw0rd',
-    database: 'election'
-  });
+  host: 'localhost',
+  // Your MySQL username,
+  user: 'root',
+  // Your MySQL password
+  password: 'P@ssw0rd',
+  database: 'election'
+});
 
-  module.exports = db;
+module.exports = db;
